b21441cc112ee5976c796ff357a6c4c0f5a29db1
Jack Andersen <jackoalan@gmail.com>
2015-04-04 14:35:32 -1000

    Added OS X filesystem-support

---
+++ include/osx_largefilewrapper.h
@@ -0,0 +1,16 @@
+#ifndef OSX_LARGEFILEWRAPPER_H
+#define OSX_LARGEFILEWRAPPER_H
+
+#if defined(__APPLE__)
+#include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+int fseeko64(FILE* fp, off_t offset, int whence);
+off_t ftello64(FILE* fp);
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+#endif // OSX_LARGEFILEWRAPPER_H
