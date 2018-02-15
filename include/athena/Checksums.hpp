5126c603b0534cf2228b657a107a50a51fa102c7
Phillip Stephens <antidote.crk@gmail.com>
2018-02-15 09:20:13 -0800

    Add CRC-64-ECMA implementation

--- include/athena/Checksums.hpp
+++ include/athena/Checksums.hpp
@@ -5,6 +5,7 @@
 
 namespace athena::Checksums
 {
+atUint64 crc64(const atUint8* data, atUint64 length, atUint64 mask = 0xFFFFFFFFFFFFFFFF, atUint64 seed = 0xFFFFFFFFFFFFFFFF);
 atUint32 crc32(const atUint8* data, atUint64 length, atUint32 mask = 0xFFFFFFFF, atUint32 seed = 0xFFFFFFFF);
 atUint16 crc16CCITT(const atUint8* data, atUint64 length, atUint16 seed = 0xFFFF, atUint16 final = 0);
 atUint16 crc16(const atUint8* data, atUint64 length);
