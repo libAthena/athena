f669191fcb2a492ff944e400c7237b5854d1cb25
Lioncash <mathew1800@gmail.com>
2019-08-24 02:31:08 -0400

    Types: Remove unnecessary macros
    These appear to be unused, and, with C++17, can be superseded with
    [[maybe_unused]] and [[deprecated]] attributes

--- include/athena/Types.hpp
+++ include/athena/Types.hpp
@@ -31,16 +31,3 @@ struct atVec3d {
 struct atVec4d {
   athena::simd<double> simd;
 };
-
-#ifndef UNUSED
-#define UNUSED(x) ((void)x)
-#endif // UNUSED
-
-#ifdef __GNUC__
-#define DEPRECATED(func) func __attribute__((deprecated))
-#elif defined(_MSC_VER)
-#define DEPRECATED(func) __declspec(deprecated) func
-#else
-#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-#define DEPRECATED(func) func
-#endif
